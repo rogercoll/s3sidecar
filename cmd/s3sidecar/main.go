756f084162b5814584f7000a53ef5bd88617c1f3
Roger Coll <rogercoll@protonmail.com>
2021-09-04 16:51:00 +0200

    feat: use env vars

--- cmd/s3sidecar/main.go
+++ cmd/s3sidecar/main.go
@@ -3,12 +3,42 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/rogercoll/s3sidecar"
 )
 
 func main() {
-	s, err := s3sidecar.NewS3Sidecar(5, "eu-west-1", "neckapps", "dictionary.txt", "/data", "/data/upload")
+	interval, ok := os.LookupEnv("INTERVAL")
+	if !ok {
+		log.Fatal("INTERVAL is not present")
+	}
+	region, ok := os.LookupEnv("AWS_REGION")
+	if !ok {
+		log.Fatal("AWS_REGION is not present")
+	}
+	bucket, ok := os.LookupEnv("S3_BUCKET")
+	if !ok {
+		log.Fatal("S3_BUCKET is not present")
+	}
+	object, ok := os.LookupEnv("S3_OBJECT")
+	if !ok {
+		log.Fatal("S3_OBJECT is not present")
+	}
+	wdir, ok := os.LookupEnv("W_DIR")
+	if !ok {
+		log.Fatal("W_DIR is not present")
+	}
+	udir, ok := os.LookupEnv("U_DIR")
+	if !ok {
+		log.Fatal("U_DIR is not present")
+	}
+	iinterval, err := strconv.Atoi(interval)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s, err := s3sidecar.NewS3Sidecar(time.Duration(int64(iinterval)), region, bucket, object, wdir, udir)
 	if err != nil {
 		log.Fatal(err)
 	}
